777ea55f46032283e913e6ebb0bcacdb365adc9f
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-09 21:58:41 +0200

    Create a custom hook to fetch the games.

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import {} from "axios";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    // handle cancelation
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []); //without [] I constantly send request to backend and it cause (Number of requests left for the period)
+
+  return { games, error };
+};
+export default useGames;
