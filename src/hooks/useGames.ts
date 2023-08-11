a210783e7cb9a496c0bece2d3574f305c3878ed8
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-11 12:54:32 +0200

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,21 +22,30 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     // handle cancelation
     const controller = new AbortController();
+
+    // Before fetching data, isLoading is true. After that(both res or err situations) I should set it to false
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        // The right place to do this is in the final method, But for some reason it does not work with strict mode turned on, So for now I have to do this in my callback. So when I get the result
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []); //without [] I constantly send request to backend and it cause (Number of requests left for the period)
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 export default useGames;
