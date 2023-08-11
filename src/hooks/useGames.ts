bef1fa6a22203a7b8bef1a4dd6ccc282af7f3e33
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-11 16:15:16 +0200

    Create a generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,4 @@
-import { useState, useEffect } from "react";
-import { CanceledError } from "axios";
-import apiClient from "../services/api-client";
-
+import useData from "./useData";
 export interface Platform {
   id: number;
   name: string;
@@ -14,38 +11,5 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // handle cancelation
-    const controller = new AbortController();
-
-    // Before fetching data, isLoading is true. After that(both res or err situations) I should set it to false
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        // The right place to do this is in the final method, But for some reason it does not work with strict mode turned on, So for now I have to do this in my callback. So when I get the result
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []); //without [] I constantly send request to backend and it cause (Number of requests left for the period)
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 export default useGames;
