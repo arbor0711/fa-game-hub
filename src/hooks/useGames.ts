928783ee6cf01822c05c750002ffb6fd2bcc9d93
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-11 15:41:33 +0200

    Fetch the genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { CanceledError } from "axios";
 import apiClient from "../services/api-client";
 
